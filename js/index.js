cea48f728cfe6e378dd4318dc57a12b63720601e
Brian_Izaki <bkizaki@outlook.com>
2020-10-14 18:15:50 -0300

    botão de voltar ao topo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function scroll(){
+function scrollSuave(){
   const linksInternos = document.querySelectorAll("a[href^='#']");
   
   function scrollToSection(event) {
@@ -6,7 +6,7 @@ function scroll(){
 
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
-
+    
     section.scrollIntoView({
       behavior: "smooth",
       block: "start",
@@ -19,4 +19,22 @@ function scroll(){
   });
 }
 
-scroll()
+function mostraMenu(){
+  document.querySelector('#menuIcon').addEventListener('click', (e) => {
+    // menu.classList.toggle('menu-web');
+    document.querySelector('.menu-web').classList.toggle('menu-mobile');
+    
+  })
+}
+
+window.addEventListener('scroll', (e)=> {
+  let pos = window.pageYOffset > 300 && window.pageYOffset < 400 
+  if (pos){
+    document.querySelector('.voltar-topo').classList.add('ativa')
+  } else if (window.pageYOffset < 300){
+    document.querySelector('.voltar-topo').classList.remove('ativa')
+  }
+})
+
+scrollSuave();
+mostraMenu();
